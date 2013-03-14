9d94d807e7b455ee3e6e967f25019030353293b5
lightluke <luke.reijmerink@hva.nl>
2013-03-14 13:27:15 +0100

    Removed Hello World
    Hello World message removed

--- LocationFinder/gen/nl/hva/proveit/locationfinder/R.java
+++ LocationFinder/gen/nl/hva/proveit/locationfinder/R.java
@@ -30,10 +30,9 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-        public static final int str_tv_location=0x7f040003;
-        public static final int str_tv_locationGPS=0x7f040004;
+        public static final int menu_settings=0x7f040001;
+        public static final int str_tv_location=0x7f040002;
+        public static final int str_tv_locationGPS=0x7f040003;
     }
     public static final class style {
         /** 
